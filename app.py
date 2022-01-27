964c73cf56bed70c868c61d79d11309d19c7f6fd
Vishnu Pillai <40285039+Vishnu3125@users.noreply.github.com>
2022-01-27 17:09:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def message():  
       return "<html><body><h1>Hi, welcome to the website</h1></body></html>"  
 if __name__ == '__main__':  
-   app.run(debug = True) 
+   app.run(host=0.0.0.0, port=9090, debug = True) 
